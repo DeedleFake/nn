f2454cfb0920c6d9310810559eb4799515c00c5f
DeedleFake <deedlefake@users.noreply.github.com>
2016-04-25 18:21:49 -0400

    Implementing more stuff.
    Train doesn't work.

--- network.go
+++ network.go
@@ -1,7 +1,6 @@
 package nn
 
 import (
-	"fmt"
 	"math/rand"
 )
 
@@ -9,20 +8,20 @@ type Network struct {
 	G      func(float64) float64
 	GPrime func(float64) float64
 
-	w [][][]float64
+	w [][]Neuron
 }
 
 func New(inputs int, layers ...int) *Network {
-	w := make([][][]float64, len(layers))
+	w := make([][]Neuron, len(layers))
 	for layer := range w {
-		w[layer] = make([][]float64, layers[layer])
+		w[layer] = make([]Neuron, layers[layer])
 		for neuron := range w[layer] {
 			prev := inputs
 			if layer > 0 {
 				prev = layers[layer-1]
 			}
 
-			w[layer][neuron] = make([]float64, prev)
+			w[layer][neuron] = make(Neuron, prev+1)
 			for input := range w[layer][neuron] {
 				w[layer][neuron][input] = rand.Float64()
 			}
@@ -37,23 +36,33 @@ func New(inputs int, layers ...int) *Network {
 	}
 }
 
-func (nn *Network) Run(data ...float64) (out [][]float64) {
-	if len(data) != len(nn.w[0][0]) {
-		panic(fmt.Errorf("Expected %v inputs, but got %v", len(nn.w[0][0]), len(data)))
+func (nn *Network) Run(data []float64) (out []float64) {
+	// TODO: This shares a lot of code with AllOutputs. Find a way to
+	// reuse the code a bit better.
+	out = make([]float64, len(data))
+	copy(out, data)
+
+	cur := make([]float64, 0, len(nn.w[0]))
+	for layer := range nn.w {
+		for neuron := range nn.w[layer] {
+			cur = append(cur, nn.G(nn.w[layer][neuron].Input(out)))
+		}
+
+		out, cur = cur, out
+		cur = cur[:0]
 	}
 
+	return out
+}
+
+func (nn *Network) AllOutputs(data []float64) (out [][]float64) {
 	out = make([][]float64, 1, len(nn.w)+1)
 	out[0] = data
 
 	for layer := range nn.w {
 		cur := make([]float64, 0, len(nn.w[layer]))
 		for neuron := range nn.w[layer] {
-			var sum float64
-			for input := range nn.w[layer][neuron] {
-				sum += nn.w[layer][neuron][input] * out[layer][input]
-			}
-
-			cur = append(cur, nn.G(sum))
+			cur = append(cur, nn.G(nn.w[layer][neuron].Input(out[layer])))
 		}
 
 		out = append(out, cur)
@@ -62,6 +71,48 @@ func (nn *Network) Run(data ...float64) (out [][]float64) {
 	return out
 }
 
-func (nn *Network) Train(in, out []float64) [][]float64 {
-	panic("Not implemented.")
+func (nn *Network) Train(in, out []float64, alpha float64) [][]float64 {
+	a := nn.AllOutputs(in)
+	delta := make([][]float64, len(nn.w))
+	outLayer := len(nn.w) - 1
+
+	for neuron := range nn.w[outLayer] {
+		d := nn.GPrime(nn.w[outLayer][neuron].Input(a[outLayer]))
+		d *= out[neuron] - a[outLayer+1][neuron]
+		delta[outLayer] = append(delta[outLayer], d)
+	}
+
+	for layer := outLayer - 1; layer >= 0; layer-- {
+		for neuron := range nn.w[layer] {
+			var sum float64
+			for next := range nn.w[layer+1] {
+				sum += nn.w[layer+1][next][neuron+1] * delta[layer+1][next]
+			}
+
+			in := nn.w[layer][neuron].Input(a[layer])
+			delta[layer] = append(delta[layer], nn.GPrime(in)*sum)
+		}
+	}
+
+	for layer := range nn.w {
+		for neuron := range nn.w[layer] {
+			nn.w[layer][neuron][0] += alpha * delta[layer][neuron]
+			for input := range nn.w[layer][neuron][1:] {
+				nn.w[layer][neuron][input+1] += alpha * a[layer][input] * delta[layer][neuron]
+			}
+		}
+	}
+
+	return a
+}
+
+type Neuron []float64
+
+func (n Neuron) Input(in []float64) float64 {
+	sum := n[0]
+	for i := range in {
+		sum += n[i+1] * in[i]
+	}
+
+	return sum
 }
--- network_test.go
+++ network_test.go
@@ -2,10 +2,32 @@ package nn_test
 
 import (
 	"github.com/DeedleFake/nn"
+	"math"
+	"math/rand"
 	"testing"
 )
 
-func TestNetwork(t *testing.T) {
+func TestAllOutputs(t *testing.T) {
 	n := nn.New(3, 5, 2)
-	t.Log(n.Run(3, 2, 1))
+	t.Log(n.AllOutputs([]float64{3, 2, 1}))
+}
+
+func TestTrain(t *testing.T) {
+	const (
+		Alpha = 1
+	)
+
+	n := nn.New(1, 10, 1)
+	for i := 0; i < 10000; i++ {
+		r := rand.Float64() * 10
+		n.Train(
+			[]float64{r},
+			[]float64{math.Sin(r)},
+			Alpha,
+		)
+	}
+
+	for i := -.5; i <= .5; i += .2 {
+		t.Logf("%v -> (%v, %v)\n", i, n.Run([]float64{i}), math.Sin(i))
+	}
 }
